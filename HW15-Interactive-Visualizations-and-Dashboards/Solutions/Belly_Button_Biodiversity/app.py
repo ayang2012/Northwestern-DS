9ba3282ca4ef074efae5dbfef944baa4280c8870
Aaron <ayang2012@gmail.com>
2019-03-30 20:33:42 -0500

    hw

--- HW15-Interactive-Visualizations-and-Dashboards/Solutions/Belly_Button_Biodiversity/app.py
+++ HW15-Interactive-Visualizations-and-Dashboards/Solutions/Belly_Button_Biodiversity/app.py
@@ -1,12 +1,9 @@
 import os
-
 import pandas as pd
 import numpy as np
-
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
